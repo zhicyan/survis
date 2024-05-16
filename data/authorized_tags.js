c2c12d5e7e2103d138e008111c81450d9960bd9c
Zhicheng Yan <97499703+zhicheng-yan@users.noreply.github.com>
2024-05-16 01:30:00 +0100

    init: first commit

---
+++ data/authorized_tags.js
@@ -0,0 +1,8 @@
+const userDefinedAuthorizedTags = {
+	"type:system": {
+		"description": "paper describing a system"
+	},
+	"retrieval:system": {
+		"description": "paper describing a system"
+	}
+}
---
+++ data/tag_categories.js
@@ -0,0 +1,14 @@
+const userDefinedTagCategories = {
+	"retrieval_data_type": {
+		"description": "type of retrieved data"
+	},
+	"retrieval_method": {
+		"description": "memory retrieval method"
+	},
+	"updating_method": {
+		"description": "memory updating method"
+	},
+	"supported_operations": {
+		"description": "supported memory updating operations"
+	},
+}
